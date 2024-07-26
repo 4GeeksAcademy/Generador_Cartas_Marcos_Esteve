75133486b21200ed65e564304f42e9ed3429038b
MarcosEstPam <marcosepamias@gmail.com>
2024-07-26 20:08:29 +0000

    avances

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 function randomCard() {
-  let numberRandom = Math.floor(Math.random() * 10 + 2);
-  document.getElementById("number").innerHTML = numberRandom;
+  let cardNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let numberRandom = Math.floor(Math.random() * cardNumbers.length);
+  document.getElementById("number").innerHTML = cardNumbers[numberRandom];
   let icons = ["♦", "♥", "♠", "♣"];
-  let iconsDivs = document.querySelectorAll("iconos");
-  let randomIcon = [Math.floor(Math.random() * 4)];
+  let iconsDivs = document.querySelectorAll(".iconosCartas");
+  let randomIcon = Math.floor(Math.random() * 4);
   iconsDivs[0].innerHTML = icons[randomIcon];
   iconsDivs[1].innerHTML = icons[randomIcon];
   //
