9cd7774ea3b544fd60540cd5bfc3227e7843b64d
MarcosEstPam <marcosepamias@gmail.com>
2024-07-26 19:38:34 +0000

    subida

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
-let numberRandom = Math.random();
-console.log(numberRandom);
+function randomCard() {
+  let numberRandom = Math.floor(Math.random() * 10 + 2);
+  document.getElementById("number").innerHTML = numberRandom;
+  let icons = ["♦", "♥", "♠", "♣"];
+  let iconsDivs = document.querySelectorAll("iconos");
+  let randomIcon = [Math.floor(Math.random() * 4)];
+  iconsDivs[0].innerHTML = icons[randomIcon];
+  iconsDivs[1].innerHTML = icons[randomIcon];
+  //
+}
