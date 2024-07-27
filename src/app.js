cee3e69b4c7eb3a4d6c4d5ec3f60fd7eb3f89746
MarcosEstPam <marcosepamias@gmail.com>
2024-07-27 18:18:37 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -2,20 +2,16 @@ function randomCard() {
   let cardNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   let numberRandom = Math.floor(Math.random() * cardNumbers.length);
   document.getElementById("number").innerHTML = cardNumbers[numberRandom];
-
-  //Bucle para los iconos. De esta forma cambio el tipo de icono y su color.
   let icons = ["♦", "♥", "♠", "♣"];
   let iconsDivs = document.querySelectorAll(".iconosCartas");
   let randomIcon = Math.floor(Math.random() * 4);
-  iconsDivs[0].innerHTML = icons[randomIcon];
-  iconsDivs[1].innerHTML = icons[randomIcon];
-  if (randomIcon === 1) {
-    iconsDivs[0].style.color = "red";
-    iconsDivs[1].style.color = "red";
-  } else {
-    iconsDivs[0].style.color = "black";
-    iconsDivs[1].style.color = "black";
+
+  for (let i = 0; i < iconsDivs.length; i++) {
+    iconsDivs[i].innerHTML = icons[randomIcon];
+    if (randomIcon === 1) {
+      iconsDivs[i].style.color = "red";
+    } else {
+      iconsDivs[i].style.color = "black";
+    }
   }
-  //
 }
-/**for (let i = 0; i < iconsDivs.length; i++) {}**/
